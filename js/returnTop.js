13d958b4fc8482d523aa728cded3c89a38f1f51c
cocoyang2017 <190024985@qq.com>
2018-03-11 00:13:29 +0800

    modify

---
+++ js/returnTop.js
@@ -0,0 +1,13 @@
+// 绑定页面滚动事件
+$(window).bind('scroll',function(){
+	var len= $(this).scrollTop();
+	if (len>500){
+		$('#returnTop').show();
+	}else{
+		$('#returnTop').hide();
+	}
+});
+// 绑定回到顶部按钮的点击事件
+$('#returnTop').click(function(){
+	$('body').animate({scrollTop:0});
+})
