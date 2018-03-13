82cb4a440af95705dc7b185995c65822a97f4566
cocoyang2017 <190024985@qq.com>
2018-03-13 10:55:31 +0800

    modify

--- js/btnIndex.js
+++ js/btnIndex.js
@@ -1,14 +1,25 @@
-// navBar 下拉菜单
+
+// mobile-navBar 下拉菜单
 $(document).ready(function(){
 	$('#dropDownBtn').click(function(){
-		$('#navBarCls').slideToggle("500");
+		$('#navBarCls').slideToggle('500');
 	})
 })
 
-// navBar 下拉菜单 下拉选项
+// mobile-navBar 下拉菜单>下拉选项
 $(document).ready(function(){
 	$('.dropdown').click(function(){
-		$('ul', this).slideToggle("100");
+		$('ul', this).slideToggle('100');
+	})
+})
+
+// mobile-footer 下拉菜单 
+$(document).ready(function(){
+	$('.ftNav_dropDown').click(function(){
+		$('.footer_navCls').slideToggle('500');
+	})
+	$('.ftCt_dropDown').click(function(){
+		$('.footer_contactCls').slideToggle('500');
 	})
 })
 
@@ -16,11 +27,11 @@ $(document).ready(function(){
 $(document).ready(function(){
 	$('#floatTools_show').click(function(){
 		$(this).css("display","none");
-		$('#floatTools_hide').animate({width:'toggle'},200,);
+		$('#floatTools_hide').stop().animate({width:'toggle'},200,);
 	})
 
 	$('#floatTools_hide').click(function(){
 		$(this).css("display","none");
-		$('#floatTools_show').animate({width:'toggle'},30);
+		$('#floatTools_show').stop().animate({width:'toggle'},30);
 	})
 })
